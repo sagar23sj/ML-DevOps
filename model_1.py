13c0dede6dabd8eb3b0e24089bdb447aca371c52
sagar23sj <sagarsonwane23@gmail.com>
2020-05-29 16:18:15 +0530

    created First ML Model

---
+++ model_1.py
@@ -0,0 +1,54 @@
+
+# baseline cnn model for mnist
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+
+# load train and test dataset
+
+(trainX, trainY), (testX, testY) = mnist.load_data()
+# reshape dataset to have a single channel
+trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+testX = testX.reshape((testX.shape[0], 28, 28, 1))
+# one hot encode target values
+trainY = to_categorical(trainY)
+testY = to_categorical(testY)
+
+# scale pixels
+
+# convert from integers to floats
+train_norm = trainX.astype('float32')
+test_norm = testX.astype('float32')
+# normalize to range 0-1
+train_norm = train_norm / 255.0
+test_norm = test_norm / 255.0
+
+# define cnn model
+def define_model_1():
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(50, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+
+	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+
+model = define_model_1()
+# fit model
+history = model.fit(trainX, trainY, epochs=5, batch_size=32, validation_data=(testX, testY), verbose=1)
+# evaluate model
+_, acc = model.evaluate(testX, testY, verbose=0)
+print('> %.3f' % (acc * 100.0))
+
+str_final_acc = str(acc*100)
+f = open("accuracy.txt", "w")
+f.write(str_final_acc)
+f.close()
+
